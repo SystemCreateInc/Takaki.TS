3ced9da08982f9460b8ceb7213e33c1317ddf9df
sakano <sakano@systemcreate-inc.com>
2023-11-29 14:58:21 +0900

    マスタ受信:新規作成

---
+++ src/MasterImporter/MasterImporterModule.cs
@@ -0,0 +1,30 @@
+﻿using DbLib.Defs;
+using DbLib.Defs.DbLib.Defs;
+using ImportLib.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreImporter
+{
+    public class MasterImporterModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RequestNavigate("ContentRegion", nameof(MainImporter), new NavigationParameters
+            {
+                { "ImportType", ImportType.Master }
+            });
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
