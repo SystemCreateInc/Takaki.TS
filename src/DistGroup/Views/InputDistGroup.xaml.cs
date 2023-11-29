308a5de121ef09ee18c1a42cc21af775a2936d57
Tanaka <tanaka@systemcreate-inc.com>
2023-11-29 16:02:09 +0900

    仕分グループ登録:雛形作成

---
+++ src/DistGroup/Views/InputDistGroup.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DistGroup.Views
+{
+    /// <summary>
+    /// InputDistGroupDlg.xaml の相互作用ロジック
+    /// </summary>
+    public partial class InputDistGroup : UserControl
+    {
+        public InputDistGroup()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/lib/DbLib/Defs/BinSumType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace DbLib.Defs
+{
+    // 配送便集計
+    public enum BinSumType
+    {
+        [Description("する")]
+        Yes = 1,
+
+        [Description("しない")]
+        No = 2,
+    }
+}
