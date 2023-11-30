3d95a67aed5a8392338003cd9f271e73fa3e37c5
Tanaka <tanaka@systemcreate-inc.com>
2023-11-30 14:34:01 +0900

    集約得意先登録:雛形作成

---
+++ src/Customer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+
+namespace Customer.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "集約得意先登録";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
---
+++ src/Customer/Views/MainCustomer.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Customer.Views
+{
+    /// <summary>
+    /// MainCustomer.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainCustomer : UserControl
+    {
+        public MainCustomer()
+        {
+            InitializeComponent();
+        }
+    }
+}
