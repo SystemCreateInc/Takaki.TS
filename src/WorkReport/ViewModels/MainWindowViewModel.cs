ac354f240655382609de3193dbc04cc899787748
Tanaka <tanaka@systemcreate-inc.com>
2023-12-07 17:58:29 +0900

    作業報告書発行:雛形作成

---
+++ src/WorkReport/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+
+namespace WorkReport.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "作業報告書発行";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
---
+++ src/WorkReport/Views/MainWorkReport.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WorkReport.Views
+{
+    /// <summary>
+    /// MainWorkReport.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWorkReport : UserControl
+    {
+        public MainWorkReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
