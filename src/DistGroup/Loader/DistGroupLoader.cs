b089537a89e00c197003600ac9b47751190ed1eb
sakano <sakano@systemcreate-inc.com>
2023-12-26 09:57:06 +0900

    仕分グループ登録:ID=7158,7159 大仕分けグループ情報表示修正
    ・join文のbugfix

--- src/DistGroup/Loader/DistGroupLoader.cs
+++ src/DistGroup/Loader/DistGroupLoader.cs
@@ -22,7 +22,7 @@ namespace DistGroup.Loader
                      + ",max(t3.CD_LARGE_GROUP_NAME) CD_LARGE_GROUP_NAME"
                      + ",max(t4.NM_KYOTEN) NM_KYOTEN"
                      + " FROM TB_DIST_GROUP t1"
-                     + " left join TB_DIST_GROUP_LARGE_GROUP t2 on t2.ID_DIST_GROUP = t2.ID_DIST_GROUP"
+                     + " left join TB_DIST_GROUP_LARGE_GROUP t2 on t2.ID_DIST_GROUP = t1.ID_DIST_GROUP"
                      + " left join TB_LARGE_GROUP t3 on t3.CD_LARGE_GROUP = t2.CD_LARGE_GROUP"
                      + " left join TB_MKYOTEN t4 on t4.CD_KYOTEN = t1.CD_KYOTEN"
                      + " group by t1.CD_KYOTEN, t1.CD_DIST_GROUP"
