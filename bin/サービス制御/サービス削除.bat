b1eb9ac88b330fc2f09e9e824f6ab1f4490a1517
scohki <ohki@mail.systemcreate-inc.com>
2023-12-28 17:17:17 +0900

    積み付けサーバー追加

--- bin/サービス制御/サービス削除.bat
+++ bin/サービス制御/サービス削除.bat
@@ -1,6 +1,6 @@
 c:\projects\Takaki.TS\bin\bin.backend\HandySvr.exe -u
 c:\projects\Takaki.TS\bin\bin.backend\Scbackend.exe -u
 
-c:\projects\Takaki.TS\bin\bin\Stowage.exe -u
+c:\projects\Takaki.TS\bin\bin\StowageSvr.exe -u
 
 pause
