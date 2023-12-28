dd1f42a8cbed4ef94f45d7d18380f1d4d04aa75c
sakano <sakano@systemcreate-inc.com>
2023-12-28 16:19:28 +0900

    集約得意先登録:ID=7163 子得意先取得失敗時、行編集確定をキャンセル
    ・Bindingディレイ時間を100に短縮
    
    ・fixme:行追加が動作しなくなる場合がある

--- src/Customer/Views/InputCustomer.xaml.cs
+++ src/Customer/Views/InputCustomer.xaml.cs
@@ -37,20 +37,21 @@ namespace Customer.Views
             }
         }
 
-        //private void dataGrid_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
-        //{
-        //    var child = e.Row.Item as ChildCustomer;
+        private void dataGrid_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            var child = e.Row.Item as ChildCustomer;
 
-        //    if(child is null)
-        //    {
-        //        return;
-        //    }
+            if (child is null)
+            {
+                return;
+            }
 
-        //    // 得意先名が取得出来ていない場合、編集を完了しない
-        //    if (child.NmTokuisaki.IsNullOrEmpty())
-        //    {
-        //        e.Cancel = true;
-        //    }
-        //}
+            // 得意先名が取得出来ていない場合、編集を完了しない            
+            if (child.NmTokuisaki.IsNullOrEmpty())
+            {
+                // fixme:実施後、行追加が動作しなくなる場合がある
+                e.Cancel = true;
+            }
+        }
     }
 }
