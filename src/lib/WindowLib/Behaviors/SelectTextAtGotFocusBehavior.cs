6406ea6a192b0e1740bbfbb0209973a9019ac02f
Hirobumi Shimada <shimada@systemcreate-inc.com>
2023-12-29 09:40:30 +0900

    0007150: 大仕分処理　【ブロック別大仕分】画面で商品を選択し『箱入数変更』ボタンを押下するとＰＧが落ちます。 説明	　 再現方法	プログラムは落ちなくなりましたが、「箱入数変更」画面を表示した時に、カーソルが変更数の前に表示され、箱入数を削除してから入力しないといけない。 手間なので変更数量を包むように（変更数を青く包まれている様に）カーソルを表示し直ぐに数値を入力できる様にして下さい。

---
+++ src/lib/WindowLib/Behaviors/SelectTextAtGotFocusBehavior.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WindowLib.Behaviors
+{
+    public class SelectTextAtGotFocusBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.GotFocus += AssociatedObject_GotFocus;
+        }
+
+        private void AssociatedObject_GotFocus(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.SelectAll();
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.GotFocus -= AssociatedObject_GotFocus;
+        }
+    }
+}
