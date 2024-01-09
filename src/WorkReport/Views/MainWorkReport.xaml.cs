b00ad01160ea12d56fd2379b30653fd67d640124
Tanaka <tanaka@systemcreate-inc.com>
2024-01-09 15:39:11 +0900

    照会・帳票プログラム全般:「/」無し入力対応のDatePickerを実装。 ・大仕分リスト発行 ・仕分・欠品リスト発行 ・積付表発行 ・作業報告書発行 ・タカハタDAS対象外仕分リスト発行

--- src/WorkReport/Views/MainWorkReport.xaml.cs
+++ src/WorkReport/Views/MainWorkReport.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using LogLib;
+using System.Windows.Controls;
 
 namespace WorkReport.Views
 {
@@ -10,6 +11,33 @@ namespace WorkReport.Views
         public MainWorkReport()
         {
             InitializeComponent();
+        }
+
+        private void DatePicker_DateValidationError(object sender, DatePickerDateValidationErrorEventArgs e)
+        {
+            if (sender == null)
+            {
+                return;
+            }
+
+            var datePicker = (DatePicker)sender;
+            var text = datePicker.Text;
+            var date = DateTime.Today.AddDays(1);
+
+            try
+            {
+                // /を含まないかつ、8文字だった場合にDateTimeに変換
+                if (!text.Contains('/') && text.Length == 8)
+                {
+                    date = DateTime.ParseExact(text, "yyyyMMdd", System.Globalization.DateTimeFormatInfo.InvariantInfo, System.Globalization.DateTimeStyles.None);
+                }
+            }
+            catch (Exception er)
+            {
+                Syslog.Error($"Date_DateValidationError:{er.Message}");
+            }
+
+            datePicker.SelectedDate = date;
         }
     }
 }
