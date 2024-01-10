ae835554649c75a35bac846c9f6db02c806b2bb2
Tanaka <tanaka@systemcreate-inc.com>
2024-01-10 16:16:51 +0900

    DatePickerExに統一

--- src/TakahataDistListPrint/Views/SelectDeliveryDateDlg.xaml.cs
+++ src/TakahataDistListPrint/Views/SelectDeliveryDateDlg.xaml.cs
@@ -17,30 +17,5 @@ namespace TakahataDistListPrint.Views
         {
             Date.Focus();
         }
-
-        private void Date_DateValidationError(object sender, DatePickerDateValidationErrorEventArgs e)
-        {
-            if (sender == null)
-            {
-                return;
-            }
-
-            var datePicker = (DatePicker)sender;
-            var text = datePicker.Text;
-
-            try
-            {
-                // /を含まないかつ、8文字だった場合にDateTimeに変換
-                if (!text.Contains('/') && text.Length == 8)
-                {
-                    var date = DateTime.ParseExact(text, "yyyyMMdd", System.Globalization.DateTimeFormatInfo.InvariantInfo, System.Globalization.DateTimeStyles.None);
-                    datePicker.SelectedDate = date;
-                }
-            }
-            catch (Exception er)
-            {
-                Syslog.Error($"Date_DateValidationError:{er.Message}");
-            }
-        }
     }
 }
