dc27a5222a3a2a3a2fb8374472e73506a48cd9c0
scohki <ohki@mail.systemcreate-inc.com>
2024-01-10 13:24:13 +0900

    0007280: 座席マッピング　納品日の入力が「yyyy/mm/dd」と必ず"/"を入力しないと日付と認識しません。「yyyymmdd」と入力しても日付として処理し「yyyy/mm/dd」で表示して下さい。

--- src/Mapping/Views/DeliveryDateDialog.xaml.cs
+++ src/Mapping/Views/DeliveryDateDialog.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using LogLib;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using WindowLib.Utils;
 
@@ -26,6 +28,47 @@ namespace Mapping.Views
         private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             Date.Focus();
+        }
+
+        private void DatePicker_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter)
+            {
+                return;
+            }
+
+            if (Keyboard.FocusedElement is UIElement newFocus && newFocus.GetType().Equals(typeof(DatePickerTextBox)))
+            {
+                EnterKeySupport.Next(sender, e);
+                e.Handled = true;
+            }
+        }
+
+        private void DatePicker_DateValidationError(object sender, DatePickerDateValidationErrorEventArgs e)
+        {
+            if (sender == null)
+            {
+                return;
+            }
+
+            var datePicker = (DatePicker)sender;
+            var text = datePicker.Text;
+            var date = DateTime.Today.AddDays(1);
+
+            try
+            {
+                // /を含まないかつ、8文字だった場合にDateTimeに変換
+                if (!text.Contains('/') && text.Length == 8)
+                {
+                    date = DateTime.ParseExact(text, "yyyyMMdd", System.Globalization.DateTimeFormatInfo.InvariantInfo, System.Globalization.DateTimeStyles.None);
+                }
+            }
+            catch (Exception er)
+            {
+                Syslog.Error($"Date_DateValidationError:{er.Message}");
+            }
+
+            datePicker.SelectedDate = date;
         }
     }
 }
