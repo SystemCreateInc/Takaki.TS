bb468cbc7a1677e8fe973ae1dec25913df96972f
Hirobumi Shimada <shimada@systemcreate-inc.com>
2024-01-12 09:44:51 +0900

    0007294: 大仕分処理　【商品スキャン】画面で商品をスキャンし『ブロック別大仕分開始(F5)』または『商品別大仕分開始(F8)』ボタンを押下して、次の画面を表示しますが、この画面で『大仕分完了』ボタンにカーソルを当てて下さい。『Enter』キーで大仕分完了する為です。

--- src/LargeDist/Views/BlockLargeDist.xaml.cs
+++ src/LargeDist/Views/BlockLargeDist.xaml.cs
@@ -11,5 +11,19 @@ namespace LargeDist.Views
         {
             InitializeComponent();
         }
+
+        private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            CompleteButton.Focus();
+        }
+
+        private void UserControl_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                CompleteButton.Command.Execute(null);
+                e.Handled = true;
+            }
+        }
     }
 }
