2219d5b7e0ed8a4caadffdce78431e8e00969509
Tanaka <tanaka@systemcreate-inc.com>
2024-01-15 09:34:32 +0900

    0007307:積付表発行　『CSV出力』で出力されたファイル内の見出しが全て英字で何の項目かわかりにくいので日本語で記載して下さい。 0007325:積付表発行　『CSV出力』で出力されるＣＳＶファイルの項目が画面に表示されている項目と同じにして下さい。
    CSVLibでのCSV出力処理を変更

--- src/lib/CsvLib/Models/CsvManager.cs
+++ src/lib/CsvLib/Models/CsvManager.cs
@@ -1,24 +1,46 @@
 ﻿using CsvHelper;
-using LogLib;
-using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Windows.Controls;
+using WindowLib.Utils;
 
 namespace CsvLib.Models
 {
     public class CsvManager
     {
-        public static void Create<T>(List<T> datas, string filePath)
+        public static void Create(DataGrid dataGrid, string fileName)
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            using (var writer = new StreamWriter(filePath, false, Encoding.GetEncoding("Shift_JIS")))
-            using (var csv = new CsvWriter(writer, new CultureInfo("ja-JP", false)))
+            using (var busy = new WaitCursor())
             {
-                csv.WriteRecords(datas);
+                using (var fs = new StreamWriter(fileName, false, Encoding.GetEncoding("SJIS")))
+                {
+                    // ヘッダー取得
+                    var header = string.Join(",", dataGrid.Columns.Select(x => x.Header.ToString()));
+
+                    // ヘッダー書き込み
+                    fs.WriteLine(header);
+
+                    // 
+                    for (int row = 0; row < dataGrid.Items.Count; row++)
+                    {
+                        var line = new List<string>();
+                        for (int column = 0; column < dataGrid.Columns.Count; column++)
+                        {
+                            var property = dataGrid.Items[row].GetType().GetProperty(dataGrid.Columns[column].SortMemberPath);
+                            var format = dataGrid.Columns[column].ClipboardContentBinding.StringFormat;
+                            var b = dataGrid.Items[row];
+                            line.Add(FormatString(property, dataGrid.Items[row], format));
+                        }
+
+                        var rowText = string.Join(",", line);
+                        fs.WriteLine(rowText);
+                    }
+                }
             }
         }
 
@@ -39,6 +61,102 @@ namespace CsvLib.Models
                     throw new System.Exception("選択したCSVを確認して下さい");
                 }
             }
+        }
+
+        // 書式設定
+        private static string FormatString(PropertyInfo? property, object? obj, string? format)
+        {
+            if (property == null || obj == null)
+            {
+                return string.Empty;
+            }
+
+            var value = property.GetValue(obj);
+
+            if (value == null)
+            {
+                return string.Empty;
+            }
+
+            if (format == null)
+            {
+                // StringFormatが指定されていない場合は、文字として返す
+                return value.ToString() ?? string.Empty;
+            }
+            else
+            {
+                // StringFormatが指定されているので、formatしてから返す
+                // DateTime型は、カスタム書式設定で指定されているため別処理でformatする
+                if (IsDateTime(property))
+                {
+                    return SetDateTimeFormatValue(value, format);
+                }
+                else if(IsTimeSpan(property))
+                {
+                    return SetTimeSpanFormatValue(value, format);
+                }
+                else
+                {
+                    var formatString = string.Format(format, value);
+                    return formatString;
+                }
+            }
+        }
+
+        private static bool IsDateTime(PropertyInfo property)
+        {
+            var methodInfo = property.GetMethod;
+            if (methodInfo == null)
+            {
+                return false;
+            }
+
+            var fullName = methodInfo.ReturnType.FullName;
+            if (fullName == null)
+            {
+                return false;
+            }
+
+            return fullName.Contains("DateTime");
+        }
+
+        private static bool IsTimeSpan(PropertyInfo property)
+        {
+            var methodInfo = property.GetMethod;
+            if(methodInfo == null)
+            {
+                return false;
+            }
+
+            var fullName = methodInfo.ReturnType.FullName;
+            if(fullName == null)
+            {
+                return false;
+            }
+
+            return fullName.Contains("TimeSpan");
+        }
+
+        // DateTimeの日付フォーマットの設定
+        private static string SetDateTimeFormatValue(object value, string format)
+        {
+            DateTime dateTime;
+
+            if (DateTime.TryParse(value?.ToString(), out dateTime))
+                return dateTime.ToString(format);
+            else
+                return "";
+        }
+
+        // TimeSpanの日付フォーマットの設定
+        private static string SetTimeSpanFormatValue(object value, string format)
+        {
+            TimeSpan timeSpan;
+
+            if (TimeSpan.TryParse(value?.ToString(), out timeSpan))
+                return timeSpan.ToString(format);
+            else
+                return "";
         }
     }
 }
