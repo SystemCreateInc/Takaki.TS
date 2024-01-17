f0d70217512c9c72f5c525da687f0a2e4796d61d
sakano <sakano@systemcreate-inc.com>
2024-01-17 14:39:02 +0900

    箱数実績データ送信ID=7339 納品日上でEnterキー押下時、OKへフォーカス移動

--- src/BoxExpoter/Views/DeliveryDateDialog.xaml.cs
+++ src/BoxExpoter/Views/DeliveryDateDialog.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using WindowLib.Utils;
 
 namespace BoxExpoter.Views
 {
@@ -15,6 +19,20 @@ namespace BoxExpoter.Views
         private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             Date.Focus();
+        }
+
+        private void Date_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter)
+            {
+                return;
+            }
+
+            if (Keyboard.FocusedElement is UIElement newFocus && newFocus.GetType().Equals(typeof(DatePickerTextBox)))
+            {
+                EnterKeySupport.Next(sender, e);
+                e.Handled = true;
+            }
         }
     }
 }
